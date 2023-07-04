9a0e1ea21362600b31f34bb7eeab9e1ad0e5f127
sasmeka <verdysas@gmail.com>
2023-07-04 14:46:28 +0700

    update

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -48,6 +48,7 @@ control.addData = async (req, res) => {
         if (req.file === undefined) return resp(res, 401, 'movie posters must be uploaded,')
         const { id_director, title, release_date, duration_hour, duration_minute, synopsis, movie_id_cast, movie_id_genre } = req.body
         const image = req.file.path
+        console.log({ id_director, title, release_date, duration_hour, duration_minute, synopsis, image, movie_id_cast, movie_id_genre })
         const result = await model.addAllData({ id_director, title, release_date, duration_hour, duration_minute, synopsis, image, movie_id_cast, movie_id_genre })
         return resp(res, 200, result)
     } catch (e) {
